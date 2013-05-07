801db4cb397d1b3ff381dda89161cff635439ef5
Equipo <Equipo@Lab612-330439>
2013-05-07 17:27:00 -0600

    Cambios realizados en el lab.

--- test/cr/ac/ucr/gimnasioa80703/data/RutinaDataTest.java
+++ test/cr/ac/ucr/gimnasioa80703/data/RutinaDataTest.java
@@ -4,28 +4,23 @@
  */
 package cr.ac.ucr.gimnasioa80703.data;
 
-import cr.ac.ucr.gimnasioa80703.dominio.Cliente;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
 /**
  *
  * @author Equipo
  */
-public class ClienteDataTest {
-
-    ClienteData clienteData;
-
+public class RutinaDataTest {
+    
+    public RutinaDataTest() {
+    }
+    
     @Before
     public void setUp() {
     }
-
+    
     @After
     public void tearDown() {
-    }
-
-    @Test
-    public void test() {
     }
 }
